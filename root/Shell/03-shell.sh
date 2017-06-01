9cfd129bb255aaef15f29a7b1e55e29eba1f95f8
fusion809 <fusion809@users.noreply.github.com>
2017-06-02 02:01:49 +1000

    Adding shell scripts

---
+++ root/Shell/03-shell.sh
@@ -0,0 +1,23 @@
+function nbash {
+    nano $HOME/.bashrc
+}
+
+function nzsh {
+    nano $HOME/.zshrc
+}
+
+function sbash {
+    source $HOME/.bashrc
+}
+
+function szsh {
+    source $HOME/.zshrc
+}
+
+function vbash {
+    vim $HOME/.bashrc
+}
+
+function vzsh {
+    vim $HOME/.zshrc
+}
