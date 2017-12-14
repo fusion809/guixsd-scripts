b3669e7ec80f3fbb5b6a13973ef6273543251bd4
Brenton Horne <brentonhorne77@gmail.com>
2017-12-14 20:06:08 +1000

    Updating

--- Shell/03-variables.sh
+++ Shell/03-variables.sh
@@ -1,2 +1,12 @@
 export PATH=$PATH:/root/.guix-profile/bin:/root/.guix-profile/sbin
 export GIO_EXTRA_MODULES="/home/fusion809/.guix-profile/lib/gio/modules${GIO_EXTRA_MODULES:+:}$GIO_EXTRA_MODULES"
+
+export GHUB=$HOME/GitHub
+export GHUBM=$GHUB/mine
+export CFG=$GHUBM/config
+export EDT=$GHUBM/editors
+export FGI=$WEB/fusion809.github.io
+export VM=$EDT/vim
+export SCR=$GHUBM/scripts
+export TH=$GHUBM/themes
+export WEB=$GHUBM/websites
